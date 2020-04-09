abd753ea272f5bebd53b2f982d33050b090b81b1
Harry Merritt <harry@merritt.es>
2020-04-09 01:14:07 +0100

    Start a http server on port 8000

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const fs      = require("fs");
 const express = require("express");
+
+
+//  Create http server
+const server = express();
+
+
+//  Expose port 8000
+const port = 8000;
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
